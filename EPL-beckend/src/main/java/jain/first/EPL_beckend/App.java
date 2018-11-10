332fc1b8521d154be09861dd28f04d3765f8cdab
ganpat chand <jainganpat45@gmail.com>
2018-11-10 15:31:21 +0530

    Project setup done

---
+++ EPL-beckend/src/main/java/jain/first/EPL_beckend/App.java
@@ -0,0 +1,13 @@
+package jain.first.EPL_beckend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
